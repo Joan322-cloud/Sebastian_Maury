67065a67942897aaefab886dfaaab6467666cb65
joan.maury <chauohei@gmail.com>
2026-06-13 15:57:33 -0500

    a

---
+++ Fundamentos de programacion/Lab 5.py
@@ -0,0 +1,50 @@
+#Ejercicio 1 Condicional
+x=input("Ingrese un número: ")
+if int(x) < 10:
+    print("pequeñito")
+if int(x) > 20:
+    print("grandote")
+print("Finalizado")
+
+print("*********************************************")
+print("*********************************************")
+
+#Ejercicio 2 Condicional
+x=5
+if x==5:
+    print("igual a 5")
+if x>4:
+    print("Mayor que 4")
+if x>=5:
+    print("Mayor igual a 5")
+if x<6:
+    print("Menor que 6")
+if x<=5:
+    print("Menor igual a 5")
+if x!=6:
+    print("No es igual a 6")
+print("Finalizado")
+
+print("*********************************************")
+print("*********************************************")
+
+#Ejercicio Indentacion
+x=5
+print("Antes 5")
+if x==5:
+    print("Es 5")
+    print("Todavia es 5")
+    print("Tercer 5")
+print("Luego 5")
+print("Antes 6")
+if x==6:
+    print("Es 6")
+    print("Todavia es 6")
+    print("Tercer 6")
+print("Luego 6")
+print("Fin")
+
+print("*********************************************")
+print("*********************************************")
+
+
